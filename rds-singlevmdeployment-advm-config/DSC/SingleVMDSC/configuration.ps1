9585c7464274fbd82c54e78366e0f942200cfb77
Jamel Achahbar <jamel.achahbar@gmail.com>
2021-04-07 17:54:44 +0200

    ok

--- rds-singlevmdeployment-advm-config/DSC/SingleVMDSC/configuration.ps1
+++ rds-singlevmdeployment-advm-config/DSC/SingleVMDSC/configuration.ps1
@@ -1,14 +1,14 @@
-param ( 
-[string]$brokerFQDN, 
-[string]$webFQDN, 
-[string]$collectionName, 
-[string]$collectionDescription 
-) 
+# param ( 
+# [string]$brokerFQDN, 
+# [string]$webFQDN, 
+# [string]$collectionName, 
+# [string]$collectionDescription 
+# ) 
   
-$localhost = [System.Net.Dns]::GetHostByName((hostname)).HostName 
+# $localhost = [System.Net.Dns]::GetHostByName((hostname)).HostName 
   
-if (!$collectionName) {$collectionName = "DK Collection"} 
-if (!$collectionDescription) {$collectionDescription = "Remote Desktop instance for accessing an isolated network environment."} 
+# if (!$collectionName) {$collectionName = "DK Collection"} 
+# if (!$collectionDescription) {$collectionDescription = "Remote Desktop instance for accessing an isolated network environment."} 
   
 Configuration RemoteDesktopSessionHost 
 { 
@@ -30,121 +30,113 @@ Configuration RemoteDesktopSessionHost
         [String]$webAccessServer 
     ) 
     Import-DscResource -Module xRemoteDesktopSessionHost 
-    if (!$connectionBroker) {$connectionBroker = $localhost} 
-    if (!$connectionWebAccessServer) {$webAccessServer = $localhost} 
+    if (!$connectionBroker) { $connectionBroker = $localhost } 
+    if (!$connectionWebAccessServer) { $webAccessServer = $localhost } 
   
     Node "localhost" 
     { 
   
-        LocalConfigurationManager 
-        { 
+        LocalConfigurationManager { 
             RebootNodeIfNeeded = $true 
         } 
   
-        WindowsFeature Remote-Desktop-Services 
-        { 
+        WindowsFeature Remote-Desktop-Services { 
             Ensure = "Present" 
-            Name = "Remote-Desktop-Services" 
+            Name   = "Remote-Desktop-Services" 
         } 
   
-        WindowsFeature RDS-RD-Server 
-        { 
+        WindowsFeature RDS-RD-Server { 
             Ensure = "Present" 
-            Name = "RDS-RD-Server" 
+            Name   = "RDS-RD-Server" 
         } 
   
-        WindowsFeature Desktop-Experience 
-        { 
+        WindowsFeature Desktop-Experience { 
             Ensure = "Present" 
-            Name = "Desktop-Experience" 
+            Name   = "Desktop-Experience" 
         } 
   
-        WindowsFeature RSAT-RDS-Tools 
-        { 
-            Ensure = "Present" 
-            Name = "RSAT-RDS-Tools" 
+        WindowsFeature RSAT-RDS-Tools { 
+            Ensure               = "Present" 
+            Name                 = "RSAT-RDS-Tools" 
             IncludeAllSubFeature = $true 
         } 
   
         if ($localhost -eq $connectionBroker) { 
-            WindowsFeature RDS-Connection-Broker 
-            { 
+            WindowsFeature RDS-Connection-Broker { 
                 Ensure = "Present" 
-                Name = "RDS-Connection-Broker" 
+                Name   = "RDS-Connection-Broker" 
             } 
         } 
   
         if ($localhost -eq $webAccessServer) { 
-            WindowsFeature RDS-Web-Access 
-            { 
+            WindowsFeature RDS-Web-Access { 
                 Ensure = "Present" 
-                Name = "RDS-Web-Access" 
+                Name   = "RDS-Web-Access" 
             } 
         } 
   
-        WindowsFeature RDS-Licensing 
-        { 
+        WindowsFeature RDS-Licensing { 
             Ensure = "Present" 
-            Name = "RDS-Licensing" 
+            Name   = "RDS-Licensing" 
         } 
     
         xRDSessionDeployment Deployment 
         { 
             SessionHost = $localhost 
-            ConnectionBroker = if ($ConnectionBroker) {$ConnectionBroker} else {$localhost} 
-            WebAccessServer = if ($WebAccessServer) {$WebAccessServer} else {$localhost} 
-            DependsOn = "[WindowsFeature]Remote-Desktop-Services", "[WindowsFeature]RDS-RD-Server" 
+            ConnectionBroker = if ($ConnectionBroker) { $ConnectionBroker } else { $localhost } 
+            WebAccessServer = if ($WebAccessServer) { $WebAccessServer } else { $localhost } 
+            DependsOn   = "[WindowsFeature]Remote-Desktop-Services", "[WindowsFeature]RDS-RD-Server" 
         } 
   
         xRDSessionCollection Collection 
         { 
-            CollectionName = $collectionName 
+            CollectionName        = $collectionName 
             CollectionDescription = $collectionDescription 
-            SessionHost = $localhost 
-            ConnectionBroker = if ($ConnectionBroker) {$ConnectionBroker} else {$localhost} 
-            DependsOn = "[xRDSessionDeployment]Deployment" 
+            SessionHost           = $localhost 
+            ConnectionBroker = if ($ConnectionBroker) { $ConnectionBroker } else { $localhost } 
+            DependsOn             = "[xRDSessionDeployment]Deployment" 
         } 
         xRDSessionCollectionConfiguration CollectionConfiguration 
         { 
-        CollectionName = $collectionName 
-        CollectionDescription = $collectionDescription 
-        ConnectionBroker = if ($ConnectionBroker) {$ConnectionBroker} else {$localhost}         
-        TemporaryFoldersDeletedOnExit = $false 
-        SecurityLayer = "SSL" 
-        DependsOn = "[xRDSessionCollection]Collection" 
+            CollectionName                = $collectionName 
+            CollectionDescription         = $collectionDescription 
+            ConnectionBroker = if ($ConnectionBroker) { $ConnectionBroker } else { $localhost }         
+            TemporaryFoldersDeletedOnExit = $false 
+            SecurityLayer                 = "SSL" 
+            DependsOn                     = "[xRDSessionCollection]Collection" 
         } 
         xRDRemoteApp Calc 
         { 
-        CollectionName = $collectionName 
-        DisplayName = "Calculator" 
-        FilePath = "C:\Windows\System32\calc.exe" 
-        Alias = "calc" 
-        DependsOn = "[xRDSessionCollection]Collection" 
+            CollectionName = $collectionName 
+            DisplayName    = "Calculator" 
+            FilePath       = "C:\Windows\System32\calc.exe" 
+            Alias          = "calc" 
+            DependsOn      = "[xRDSessionCollection]Collection" 
         } 
         xRDRemoteApp Mstsc 
         { 
-        CollectionName = $collectionName 
-        DisplayName = "Remote Desktop" 
-        FilePath = "C:\Windows\System32\mstsc.exe" 
-        Alias = "mstsc" 
-        DependsOn = "[xRDSessionCollection]Collection" 
+            CollectionName = $collectionName 
+            DisplayName    = "Remote Desktop" 
+            FilePath       = "C:\Windows\System32\mstsc.exe" 
+            Alias          = "mstsc" 
+            DependsOn      = "[xRDSessionCollection]Collection" 
         } 
  
         xRDRemoteApp WordPad 
         { 
-        CollectionName = $collectionName 
-        DisplayName = "WordPad" 
-        FilePath = "C:\Program Files\Windows NT\Accessories\wordpad.exe" 
-        Alias = "wordpad" 
-        DependsOn = "[xRDSessionCollection]Collection" 
+            CollectionName = $collectionName 
+            DisplayName    = "WordPad" 
+            FilePath       = "C:\Program Files\Windows NT\Accessories\wordpad.exe" 
+            Alias          = "wordpad" 
+            DependsOn      = "[xRDSessionCollection]Collection" 
         } 
         xRDRemoteApp CMD
         { 
-        CollectionName = $collectionName 
-        DisplayName = "CMD" 
-        FilePath = "C:\windows\system32\cmd.exe" 
-        Alias = "cmd" 
-        DependsOn = "[xRDSessionCollection]Collection" 
+            CollectionName = $collectionName 
+            DisplayName    = "CMD" 
+            FilePath       = "C:\windows\system32\cmd.exe" 
+            Alias          = "cmd" 
+            DependsOn      = "[xRDSessionCollection]Collection" 
         } 
     } 
 } 
